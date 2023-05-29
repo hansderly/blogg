b86fb4e7ef1e349c71c4d1c8f2a2f5b3d6b7552f
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-28 20:13:19 -0400

    Add method to increment and decrement comments_counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :post
   after_save :increment_comment
 
+  def increment_comment
+    post.increment!(:comments_count)
+  end
+
+  def decrement_comment
+    post.decrement!(:comments_count)
+  end
 end
