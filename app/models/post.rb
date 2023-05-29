41a76e339f565dc858c65c9a238b3c666ff71f68
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-28 20:16:00 -0400

    Add method to increment and decrement posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  after_save :increment_post
+
+  def increment_post
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_post
+    author.decrement!(:posts_counter)
+  end
 end
