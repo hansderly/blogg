f840ffe091a51ad09efa1ea702994d9b32abf1bd
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-28 20:19:16 -0400

    Add method to increment  likes_counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_save :increment_like
+
+  def increment_like
+    post.increment!(:likes_counter)
+  end
+
 end
