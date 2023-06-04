0bc620038244db26366fe6a7476f9843f02e10dc
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-04 10:42:44 -0400

    Add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+class UserTest < ActiveSupport::TestCase
+  describe User, type: :model do
+    it 'is not valid without a name' do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid if posts_counter is not an integer greater than or equal to zero' do
+      user = User.new(posts_counter: -1)
+      expect(user).to_not be_valid
+    end
+
+    it 'is valid if posts_counter is an integer greater than or equal to zero' do
+      user = User.new(name: 'test', posts_counter: 0)
+      expect(user).to be_valid
+    end
+  end
+end
