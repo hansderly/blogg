f02ac9709ea887d5a7ae246017ebb7cbf4f8a9c6
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-04 10:45:24 -0400

    Specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,10 +11,5 @@ class UserTest < ActiveSupport::TestCase
       user = User.new(posts_counter: -1)
       expect(user).to_not be_valid
     end
-
-    it 'is valid if posts_counter is an integer greater than or equal to zero' do
-      user = User.new(name: 'test', posts_counter: 0)
-      expect(user).to be_valid
-    end
   end
 end
