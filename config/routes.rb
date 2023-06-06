b6e3b0acbc35eab36950144338cabe658e2f1f25
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-06 08:32:33 -0400

    Add alias for routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   get "/posts/new", to: "posts#new", as: :new_post
   post "/posts/create", to: "posts#create", as: :posts
-  post "/users/:user_id/posts/:id/like_a_post", to: "likes#like_a_post"
-  post "/users/:user_id/posts/:id/comment_a_post", to: "comments#comment_a_post"
+  post "/users/:user_id/posts/:id/like_a_post", to: "likes#like_a_post", as: :like_a_post
+  post "/users/:user_id/posts/:id/comment_a_post", to: "comments#comment_a_post", as: :comment_a_post
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
