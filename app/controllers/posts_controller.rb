6d96b3ed905b803c9b9af2038c278d8210828797
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-05 23:38:52 -0400

    Set comment and like counter to zero

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
 
     if @post.save
       redirect_to user_posts_path(current_user)
