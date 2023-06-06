d0e6aa5b94e349bec6fabb08a51003bcfb34837e
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-05 23:40:09 -0400

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/posts/new", to: "posts#new", as: :new_post
+  post "/posts/create", to: "posts#create", as: :posts
+  post "/users/:user_id/posts/:id/add_like", to: "likes#add_like"
+  post "/users/:user_id/posts/:id/add_comment", to: "comments#add_comment"
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
