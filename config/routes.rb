731ec37f50377aa192a3d1ecc88af6d863456218
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-06 08:17:40 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   get "/posts/new", to: "posts#new", as: :new_post
   post "/posts/create", to: "posts#create", as: :posts
-  post "/users/:user_id/posts/:id/add_like", to: "likes#add_like"
-  post "/users/:user_id/posts/:id/add_comment", to: "comments#add_comment"
+  post "/users/:user_id/posts/:id/like_a_post", to: "likes#like_a_post"
+  post "/users/:user_id/posts/:id/comment_a_post", to: "comments#comment_a_post"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
