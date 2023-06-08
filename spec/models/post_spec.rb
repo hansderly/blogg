b9847d5238a4f321f04dd287a69a2038e8bdd94a
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-07 20:29:18 -0400

    Add test for post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,11 @@ class PostTest < ActiveSupport::TestCase
   describe Post, type: :model do
     author = User.new(name: 'user1')
 
+    let(:author_two) { User.create(name: 'user2', posts_counter: 0) }
+    let(:post) do
+      Post.create(author: author_two, title: 'title', text: 'text', likes_counter: 0, comments_counter: 0)
+    end
+
     describe 'Title' do
       it 'fail if e,pty' do
         post = Post.new(title: nil, comments_counter: 1, likes_counter: 1, author:)
@@ -19,6 +24,12 @@ class PostTest < ActiveSupport::TestCase
         post = Post.new(title: 'a' * 251, comments_counter: 1, likes_counter: 1, author:)
         expect(post).to_not be_valid
       end
+    end
+
+
+    it 'last_five_comments should return five comments' do
+      6.times { Comment.create(text: 'c1', post: post, author: author_two) }
+      expect(post.last_five_comments.length).to equal(5)
     end
   end
 end
