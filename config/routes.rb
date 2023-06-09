f2578f58b6a29c28441dc7cccd173e3c85d81bdb
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-08 23:08:36 -0400

    Add routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :users, only: :show do
+        resources :posts, only: [:index], shallow: true do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
   get "/posts/new", to: "posts#new", as: :new_post
   post "/posts/create", to: "posts#create", as: :posts
   post "/users/:user_id/posts/:id/like_a_post", to: "likes#like_a_post", as: :like_a_post
