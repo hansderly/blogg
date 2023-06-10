ebf5441bcdccc1df4a7dde967e5194780b3dd1c5
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-06-09 23:41:33 -0400

    Fix typos

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PostsController < Api::V1::BaseController
   skip_before_action :authenticate, only: :index
 
   def index
-    @posts = Post.where(author: User.find(params[:user_id])
+    @posts = Post.where(author: User.find(params[:user_id]))
 
     render json: @posts
   end
